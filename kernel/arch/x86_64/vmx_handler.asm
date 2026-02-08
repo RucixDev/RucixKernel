87b13a748bc52406bdf3e749e73794bfc9d8be04
Patryk <patryk@rucix.dev>
2026-02-08 19:40:47 +0100

    Upload vmx_handler.asm via CLI

---
+++ kernel/arch/x86_64/vmx_handler.asm
@@ -0,0 +1,75 @@
+
+global vmx_exit_handler
+extern vmx_handle_exit
+
+vmx_exit_handler:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+    
+
+    mov rdi, rsp
+    
+
+    mov rbp, rsp
+    and rsp, -16
+    
+    call vmx_handle_exit
+    
+    mov rsp, rbp
+    
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rbp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    
+    vmresume
+    jmp $
+
+global vmx_launch_asm
+vmx_launch_asm:
+
+
+
+    xor rax, rax
+    xor rbx, rbx
+    xor rcx, rcx
+    xor rdx, rdx
+    xor rsi, rsi
+    xor rdi, rdi
+    xor rbp, rbp
+    xor r8, r8
+    xor r9, r9
+    xor r10, r10
+    xor r11, r11
+    xor r12, r12
+    xor r13, r13
+    xor r14, r14
+    xor r15, r15
+    
+    vmlaunch
+
+    ret
